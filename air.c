8c589b2ff2e8296718531572839c1091107295fc
kdp <kdp7584@gmail.com>
2022-05-04 23:49:13 +0900

    Create air.c

---
+++ air.c
@@ -0,0 +1,13 @@
+/************************************************
+ *
+ *  air.c
+ *
+ ************************************************/
+
+#include <stdio.h> // Standard Input/Output Library
+#include <fcntl.h> // File Control Options
+#include <sys/types.h> // Data Types
+
+// C Standard Library
+#include <ctype.h> // Functions that are useful for testing and mapping characters
+#include <string.h> // One variable type, One macro, and Various functions for manipulating arrays of characters
