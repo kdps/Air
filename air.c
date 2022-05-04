1f8c0a2d80de1a656113b7b4f7d9b9a1b3a1bdf1
kdp <kdp7584@gmail.com>
2022-05-04 23:59:50 +0900

    Update air.c

--- air.c
+++ air.c
@@ -6,8 +6,14 @@
 #include "air.h"
 
 #include <stdio.h> // Standard Input/Output Library
+#include <stdlib.h>
+
+#include <unistd.h>
+
 #include <fcntl.h> // File Control Options
+
 #include <sys/types.h> // Data Types
+#include <sys/stat.h>
 
 // C Standard Library
 #include <ctype.h> // Functions that are useful for testing and mapping characters
